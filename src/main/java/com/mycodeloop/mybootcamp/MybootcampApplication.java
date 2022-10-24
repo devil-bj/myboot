51befd85f288150a597411e14447be3a81127a5f
yajib16 <50978166+yajib16@users.noreply.github.com>
2022-10-24 00:19:08 -0500

    My first commit

---
+++ src/main/java/com/mycodeloop/mybootcamp/MybootcampApplication.java
@@ -0,0 +1,13 @@
+package com.mycodeloop.mybootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycodeloop/mybootcamp/MybootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycodeloop.mybootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
